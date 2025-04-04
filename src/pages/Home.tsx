1c3d24194f7bb0c0da2d9fe91a536b623ae02cbf
Murat Gulcelik <123703549+mrtglclk@users.noreply.github.com>
2025-04-04 14:25:16 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-black text-white">
-      <h1 className="text-5xl">Welcome to Murat Gulcelik Music</h1>
+      <h1 className="text-5xl">Welcome to h Murat Gulcelik Music</h1>
     </div>
   );
 };
